07675ebf55a5d1edd057f55098eb07384c8a95d6
liudonghua123 <liudonghua123@gmail.com>
2023-02-16 20:30:31 +0800

    update the update.sh bash script, add timestamp and copy the output to log file

--- update.sh
+++ update.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
+log_file=app.log
 # update the code
-git pull
+echo [`date "+%Y-%m-%d %H:%M:%S"`] update the code | tee -a $log_file
+git pull | tee -a $log_file
 
 # build the frontend
+echo [`date "+%Y-%m-%d %H:%M:%S"`] build the frontend
 cd jeecgboot-vue3
-yarn build
+yarn build | tee -a $log_file
 cd ..
 rm -rf html
+# type cp: cp is aliased to `cp -i', use \cp to use the real cp
 \cp -r jeecgboot-vue3/dist html
 
 # build the backend
+echo [`date "+%Y-%m-%d %H:%M:%S"`] build the backend
 cd jeecg-boot
-mvn clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true | tee -a $log_file
 cd ..
 rm -rf device_reservation_mgmt.jar
 \cp jeecg-boot/jeecg-module-system/jeecg-system-start/target/jeecg-system-start-3.4.4.jar device_reservation_mgmt.jar
 \cp application-dev.yml jeecg-boot/jeecg-module-system/jeecg-system-start/target/
-sudo service device_reservation_mgmt restart
+
+# restart the backend
+echo [`date "+%Y-%m-%d %H:%M:%S"`] restart the backend
+sudo service device_reservation_mgmt restart | tee -a $log_file
 # view the log
-# sudo tail -f /var/log/device_reservation_mgmt.log
+echo view the log using \"sudo tail -f /var/log/device_reservation_mgmt.log\"
 
 # reload nginx
-sudo service nginx reload
+echo [`date "+%Y-%m-%d %H:%M:%S"`] reload nginx
+sudo service nginx reload | tee -a $log_file
